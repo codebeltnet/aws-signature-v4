00781cdab3a81244537852cce71a3c70734d3a09
Michael Mortensen <michael@geekle.io>
2024-10-05 16:17:16 +0200

    :memo: updated DocFX

--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
 $version = minver -i -t v -v w
-docker tag classlibrary1-docfx:$version yourbranding/classlibrary1:$version
-docker push yourbranding/classlibrary1:$version
+docker tag awssignature4-docfx:$version jcr.codebelt.net/geekle/awssignature4-docfx:$version
+docker push jcr.codebelt.net/geekle/awssignature4-docfx:$version
